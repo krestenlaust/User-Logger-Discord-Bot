34029e33bf7eced585a80ba6262bcab00682ce12
kres0345 <kressckerl@gmail.com>
2018-07-29 20:10:27 +0200

    Added new feutures
    Added option to seperatate messages with a specific char. Also added option to ignore messages starting with specific char

--- loggerbot.py
+++ loggerbot.py
@@ -7,7 +7,9 @@ logFolder = "Userlogs" #Folder with log files.
 fileExt = ".log" #The extension of the userlogs, if changed after logs are created multiple files will be created.
 avoidMsg = ["Image made with","Loading...","t!"] #Avoids messages containing one of these substrings.
 cmdPrefix = '$' #The prefix to the commands.
+muteChar = '£' #Place this char infront of message to not store message.
 tokenKey = "NDYwMDQwOTExNDg1OTI2nDY1.DG_yJw.iY19n6GzjWC-IqT1slIgySUbyAM" #<< Not actual token, just an example.
+seperationChar = '\n' #The charactor that seperates new messages. Blank for none.
 
 client = discord.Client()
 print("Initiating logger")
@@ -15,7 +17,9 @@ print("Initiating logger")
 async def on_ready():
     print("Logged in as: "+client.user.name)
     print(client.user.id)
-    print("Command prefix: "+cmdPrefix)
+    print("Command prefix: "+repr(cmdPrefix))
+    print("Seperation char: "+repr(seperationChar))
+    print("Mute char: "+repr(muteChar))
     print("Log folder: {0}, log file extension: {1}".format(logFolder, fileExt))
     print("-------------")
     if not(isdir(logFolder)):
@@ -24,22 +28,24 @@ async def on_ready():
 @client.event
 async def on_message(message):
     try:
-        i=0;msgContained=False;
+        i=0;msgContained=False;Status="[Not Logged] ";Nickname="";
         while i<len(avoidMsg):
             if(avoidMsg[i] in message.content):
                 print("Avoiding message: {0} | By {1}".format(message.content.replace("\n"," "), message.author.name))
                 msgContained=True
             i+=1
         if not(msgContained):
-            Targetfile=join(logFolder,message.author.name+fileExt)
-            MsgText=sub("[^a-zA-Z -<>:]+", "", message.content.replace("\n"," "))+' '
+            Targetfile=join(logFolder,message.author.name+fileExt) #Combines paths with relevant variables.
+            MsgText=sub("[^a-zA-Z -<>.,':]+", "", message.content.replace("\n"," "))+' ' #Sorts out not supported-chars.
             if(len(MsgText)<1):
                 return False
-            #MsgText=message.content.replace("\n"," ")+' ' #Replaced with ASCIInizing re.sub function.
-            #open(Targetfile,'a').write(MsgText) #Replaced with aiofiles, for async support and support writing multiple files at a time.
-            async with aiofiles.open(Targetfile, mode='a') as f:
-                await f.write(MsgText)
-            print('Wrote: "{0}" to "{1}"'.format(MsgText,str(Targetfile)))
+            if not(message.content[0] == muteChar):
+                Status = ""
+                async with aiofiles.open(Targetfile, mode='a') as f:
+                    await f.write(MsgText+seperationChar)
+            if not(message.author.name == message.author.display_name):
+                Nickname = "({0})".format(str(message.author.display_name))
+            print('{1}{2} {3}said: {0}'.format(MsgText,str(message.author.name),Nickname,Status))
     except Exception as e:
         print("Exception: "+str(e))
     
